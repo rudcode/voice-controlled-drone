e7a8c37e897b9560780aa20518b456236b7f7411
aldwinakbar <aldwinakbar@gmail.com>
2015-11-18 12:01:14 +0700

    added working tcp video streamer

--- hardware/non-ros/video_feed/receiver.cpp
+++ hardware/non-ros/video_feed/receiver.cpp
@@ -1,58 +1,80 @@
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <iostream>
 #include <stdio.h>
-#include <pthread.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-// ./server_udp 7891
 
 using namespace std;
 using namespace cv;
-
-	int udpSocket, nBytes, server_port;
+ 
+int main(int argc , char *argv[])
+{
 	char buffer[100000];
-	struct sockaddr_in serverAddr, clientAddr;
-	struct sockaddr_storage serverStorage;
-	socklen_t addr_size, client_addr_size;
 	Mat img_stream;
 	
-int main(int argc, char** argv){
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[2000];
+    int port_number = 50005;
+     
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1){
+        printf("Could not create socket");
+    }
 	
-	/* Inisialisasi Socket Server */
-	server_port = atoi(argv[1]);					// Port Server
-	udpSocket = socket(AF_INET, SOCK_DGRAM, 0);		// membuka koneksi UDP
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(server_port);		// membuka port
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");	
-	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
-	bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-	addr_size = sizeof serverStorage;
-	/* Inisialisasi Socket Server */
-	
-	namedWindow("Server Streamer", WINDOW_NORMAL);
-	
-  
-	while(1){
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port_number);
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0){
+		perror("bind failed. Error");
+        return 1;
+    }   
+    listen(socket_desc , 3);
     
-		nBytes = recvfrom(udpSocket,buffer,100000,0,(struct sockaddr *)&serverStorage, &addr_size); // Menerima Data dari client
-		
-		cout << nBytes << " Bytes Received" << endl;
-		Mat img_buff(Size(640, 360), CV_8U, buffer);				// deklarasi tempat untuk frame gambar
-		img_stream = imdecode(img_buff, CV_LOAD_IMAGE_COLOR);		// men decode image data yang diterima
-		if (!img_stream.empty()){
-			imshow("Server Streamer", img_stream);					// menampilkan gambar
+	while(1){
+		puts("Waiting for incoming connections...");
+		c = sizeof(struct sockaddr_in);
+		 
+		client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+		if (client_sock < 0){
+			perror("accept failed");
+			return 1;
 		}
-		
-		if (waitKey(30) == 27){
-			cout << "esc key is pressed by user" << endl;
-			break; 
-		}
+		puts("Connection accepted");
+		  namedWindow("Server Streamer", WINDOW_NORMAL); 
 
+		while( (read_size = recv(client_sock , client_message , 100000 , 0)) > 0 ){
+			cout << read_size << " Bytes Received" << endl;
+			
+			Mat img_buff(Size(640, 360), CV_8U, client_message);				// deklarasi tempat untuk frame gambar
+			img_stream = imdecode(img_buff, CV_LOAD_IMAGE_COLOR);		// men decode image data yang diterima
+			if (!img_stream.empty()){
+				imshow("Server Streamer", img_stream);					// menampilkan gambar
+			}
+			
+			if (waitKey(30) == 27){
+				cout << "esc key is pressed by user" << endl;
+				break; 
+			}
+				
+		}
+		 
+		if(read_size == 0){
+			puts("Client disconnected");
+			fflush(stdout);
+		}
+		else if(read_size == -1){
+			perror("recv failed");
+		}
 	}
-
-  return 0;
+	close(socket_desc);
+     
+    return 0;
 }
--- hardware/non-ros/video_feed/transmiter.cpp
+++ hardware/non-ros/video_feed/transmiter.cpp
@@ -1,46 +1,61 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <sys/time.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stdio.h>
 #include <fstream>
-#include <arpa/inet.h>
 #include <vector>
-#include <unistd.h>
-
 
 using namespace std;
 using namespace cv;
-
-char encoded_image[100000];
-vector<uchar> buff;
-vector<int> param = vector<int>(2);
-int clientSocket, portNum, nBytes, bytes, x, server_port;
-struct sockaddr_in serverAddr;
-struct sockaddr_storage serverStorage;
-socklen_t addr_size;
-char* server_ip;
-
-int main(int argc, char** argv){
-
-	// ./client_udp 127.0.0.1 7891 90 0  IP Port Quality Webcam
-	param[0] = CV_IMWRITE_JPEG_QUALITY;			// set tipe encoding 
-	param[1] = atoi(argv[3]);					// set kualitas encoding
-	
-	/* Inisialisasi Socket Client */
-	server_ip 	= argv[1];						// IP Server yang dituju
-	server_port = atoi(argv[2]);				// Port Server yang dituju
-	clientSocket = socket(PF_INET, SOCK_DGRAM, 0);	// membuka koneksi UDP
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(server_port);		// Membuka Port Server
-	serverAddr.sin_addr.s_addr = inet_addr(server_ip);	// Membuka IP Server
-	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
-	addr_size = sizeof serverAddr;	
-	/* Inisialisasi Socket Client */
-
-	VideoCapture cap(atoi(argv[4]));			// Membuka Kamera
+ 
+int main(int argc , char *argv[])
+{
+	// Declare Variable 
+    int sock,nBytes,x;
+    struct sockaddr_in server;
+    char message[100000] , server_reply[2000];
+    vector<uchar> buff;
+    vector<int> param = vector<int>(2); 
+    
+    
+    // declare image sent configuration value
+    param[0] = CV_IMWRITE_JPEG_QUALITY;			// set tipe encoding 
+	param[1] =90;					// set kualitas encoding
+    
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(50005);
+ 
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+     
+    puts("Connected\n");
+    
+    // ######################### Start video transmit ########################### 
+    VideoCapture cap(0);			// Membuka Kamera
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);		// Set Lebar gambar
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 360);		// Set tinggi Gambar
 
@@ -54,20 +69,28 @@ int main(int argc, char** argv){
 	Mat image_capture;
 
 	while(1){
-    cap >> image_capture;				// mengambil gambar dari kamera
-    
-    cvtColor(image_capture, image_capture, CV_8U);
+		cap >> image_capture;				// mengambil gambar dari kamera
 		
-    imencode(".jpeg",image_capture, buff,param);	// meng-encode gambar
-          
-    for (x = 0; x < buff.size(); x++){				// merubah data gambar dari tipe vector ke array
-     encoded_image[x] = buff[x];
+		cvtColor(image_capture, image_capture, CV_8U);
+			
+		imencode(".jpeg",image_capture, buff,param);	// meng-encode gambar
+			  
+		for (x = 0; x < buff.size(); x++){				// merubah data gambar dari tipe vector ke array
+			message[x] = buff[x];
+		}
+		
+		nBytes = buff.size();
+		
+		if( send(sock , message , nBytes , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+	
+		cout<<buff.size()<<" Bytes Sent"<<endl;
+		memset(&message[0], 0, nBytes);			// men-clear buffer gambar
 	}
-    
-	nBytes = buff.size();
-	bytes = sendto(clientSocket,encoded_image,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);	// mengirim data ke server
-    cout<<buff.size()<<" Bytes Sent"<<endl;
-    memset(&encoded_image[0], 0, nBytes);			// men-clear buffer gambar
-	}
-  return 0;
+     
+    close(sock);
+    return 0;
 }
