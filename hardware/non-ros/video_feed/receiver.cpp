09fe311ad7041d6347dbc6496962db7e13c78c50
crcdor <rudynurhadi@gmail.com>
2015-11-23 13:38:08 +0700

    start flag to send image

--- hardware/non-ros/video_feed/receiver.cpp
+++ hardware/non-ros/video_feed/receiver.cpp
@@ -22,39 +22,39 @@ using namespace cv;
 int main(int argc , char *argv[])
 {
 	// ############ Start OpenCV Variable ############ 
-	int nBytes,x;
-    char video_data[100000];
-    vector<uchar> buff;
-    vector<int> param = vector<int>(2);      
+	int nBytes, x;
+	char video_data[100000];
+	vector<uchar> buff;
+	vector<int> param = vector<int>(2);     
 	Mat image_capture;  
-    param[0] = CV_IMWRITE_JPEG_QUALITY;			// set tipe encoding 
-	param[1] =90;								// set kualitas encoding
+	param[0] = CV_IMWRITE_JPEG_QUALITY;			// set tipe encoding 
+	param[1] = 50;						// set kualitas encoding
 	// ############ End OpenCV Variable ############ 
-	
+
 	// ############  Start Create Socket  ############ 
-    int socket_desc , client_sock , c , read_size;
-    struct sockaddr_in server , client;
-    char client_message[2000];
-    int port_number = 50005;
-     
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1){
-        printf("Could not create socket");
-    }
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(port_number);
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0){
+	int socket_desc , client_sock , c , read_size;
+	struct sockaddr_in server , client;
+	char client_message[2000];
+	int port_number = 50005;
+
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+		if (socket_desc == -1){
+		printf("Could not create socket");
+	}
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(port_number);
+	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0){
 		perror("bind failed. Error");
-        return 1;
-    }   
-    listen(socket_desc , 3);
-    
-    // ############  End Create Socket  ############ 
-    
-    
-    // ############  Start Video Init ############   
-    VideoCapture cap(0);			// Membuka Kamera
+		return 1;
+	}   
+	listen(socket_desc , 3);
+
+	// ############  End Create Socket  ############ 
+
+
+	// ############  Start Video Init ############   
+	VideoCapture cap(0);			// Membuka Kamera
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);		// Set Lebar gambar
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 360);		// Set tinggi Gambar
 	if(!cap.isOpened()){
@@ -62,8 +62,7 @@ int main(int argc , char *argv[])
 		return -1;
 	}
 	// ############  End Video Init ############  
-
-    // Keep waiting for connection
+	// Keep waiting for connection
 	while(1){
 		puts("Waiting for incoming connections...");
 		c = sizeof(struct sockaddr_in);
@@ -80,29 +79,52 @@ int main(int argc , char *argv[])
 			
 			//Send the message back to client
 			if(client_message[0] == 'v' && client_message[1] == 'f'){
-				cap >> image_capture;									// mengambil gambar dari kamera
+				cap >> image_capture;					// mengambil gambar dari kamera
 				cvtColor(image_capture, image_capture, CV_8U);
-					
-				imencode(".jpeg",image_capture, buff,param);			// meng-encode gambar
-					  
-				for (x = 0; x < buff.size(); x++){						// merubah data gambar dari tipe vector ke array
+				
+				imencode(".jpeg", image_capture, buff, param);		// meng-encode gambar
+			
+				nBytes = buff.size();
+
+				for (x = 0; x < nBytes; x++){				// mengubah data gambar dari tipe vector ke array
 					video_data[x] = buff[x];
 				}
-				
-				nBytes = buff.size();
-				
-				if(send(client_sock , video_data , nBytes , 0) < 0){	// send encoded image to client
-					puts("Send failed");
+			
+				char byteBuffer[20];
+				sprintf(byteBuffer, "*AI\n%d\n", nBytes);
+				if(send(client_sock, byteBuffer, sizeof(byteBuffer), 0) < 0) {	// send start flag and size
+					cout << "Send start flag and size failed " << endl;
+					return 1;
+				}			
+				if(send(client_sock, video_data, nBytes, 0) < 0) {		// send encoded image to client
+					cout << "Send image data failed" << endl;
+					return 1;
+				}
+				if(send(client_sock, "\n", 1, 0) < 0) {				// end string
+					cout << "Send end string failed" << endl;
 					return 1;
 				}
 			
-				cout << buff.size() << " Bytes Sent" <<endl;
-				memset(&video_data[0], 0, nBytes);						// men-clear buffer gambar
+				cout << nBytes << " Bytes length" <<endl;
+				memset(&video_data[0], 0, nBytes);				// men-clear buffer gambar
+				//usleep(50000);
 			}
-			
-			else
-			send(client_sock , "ERROR\0" , 7,0);						// if client_message not vf (video fetch)
-			
+			else if(client_message[0] == 's' && client_message[1] == 'c'){
+				if(send(client_sock, "OK\n", sizeof("OK\n")-1, 0) < 0) {	// send ok
+					cout << "Send ok failed" << endl;
+					return 1;
+				}
+			}
+			else if(client_message[0] == 'd' && client_message[1] == 's'){
+				if(send(client_sock, "Drone Status\n", sizeof("Drone Status\n")-1, 0) < 0) {	// send Drone Status
+					cout << "Send ok failed" << endl;
+					return 1;
+				}
+			}
+			else {
+				send(client_sock, "ERROR\n", 6, 0);			// if client_message not vf (video fetch)
+				cout << "ERROR" <<endl;
+			}
 		}
 		
 		// ############  End Receiving Data ############ //
@@ -111,7 +133,7 @@ int main(int argc , char *argv[])
 			puts("Client disconnected");
 			fflush(stdout);
 		}
-		else if(read_size == -1){
+		else if(read_size <= -1){
 			perror("recv failed");
 		}
 	}
@@ -119,3 +141,4 @@ int main(int argc , char *argv[])
      
     return 0;
 }
+
